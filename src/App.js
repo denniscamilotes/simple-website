ef1e2aeecac28a36e8ebb9efcf0b6dc1cdb4f455
denniscamilotes <30547283+denniscamilotes@users.noreply.github.com>
2021-05-12 22:17:52 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import SiteInfo from './components/main';
+import Footer from './compontents/footer';
+
+function App() {
+  return (
+ 
+        <h1>
+         React App
+        </h1>
+       
+   
+  );
+}
+
+export default App;
