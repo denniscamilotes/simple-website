27ad54dca0b6b45d087e8f29191669ca18b7f4c4
denniscamilotes <30547283+denniscamilotes@users.noreply.github.com>
2021-05-12 22:17:07 +0800

    Add files via upload

---
+++ components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+
+            <footer>
+                <div className="social">
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-youtube-square"></i>
+                <i class="fab fa-facebook-f"></i>
+                </div>
+                <div className="copyright">
+                        Copyright &copy; 2020. UI Syntax
+                </div>
+
+            </footer>
+
+);
+
+}
+
+export default Footer;
---
+++ components/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const SiteInfo = () => {
+
+return(
+<div className="site-info">
+
+    <div className="title">   
+        IU Syntax
+
+    </div>
+    <div className="sub-title">
+        Web Design | Mobile Apps
+    </div>
+
+</div>
+
+);
+
+}
+
+export default SiteInfo;
